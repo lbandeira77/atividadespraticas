2581ba2e9e9be357d97d1f826983733f951d1c11
lbandeira77 <71645449+lbandeira77@users.noreply.github.com>
2020-12-03 17:13:30 -0300

    Add files via upload

---
+++ Aula9/src/controle/Programa.java
@@ -0,0 +1,10 @@
+package controle;
+
+import face.JanelaTime;
+
+public class Programa {
+	public static void main(String[] args) {
+		JanelaTime jt = new JanelaTime();
+		jt.setVisible(true);
+	}
+}
